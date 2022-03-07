a9866c93d8cb178288ca5e62567386f4f62c9f45
Chubkey <69558928+Ravager6969@users.noreply.github.com>
2022-03-07 10:27:21 -0800

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv/config');
 //https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe/43881141#43881141
 //app.use(cors({origin: '*', credentials: true, optionsSuccessStatus: 200,}));
 
+app.use(cors());
+
 app.use(express.static(__dirname));
 
 //Middlewares
